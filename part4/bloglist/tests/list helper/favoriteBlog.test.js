54f0b205f5ba7553a797acc0cbbbf97292085f56
cxnzensei <nishitpatil3231@gmail.com>
2021-12-10 00:33:24 +0530

    jest

---
+++ part4/bloglist/tests/list helper/favoriteBlog.test.js
@@ -0,0 +1,6 @@
+const favoriteBlog = require('../utils/list_helper').favoriteBlog
+const blogs = require('./testData').blogs
+
+test('Blog with most likes', () => {
+  expect(favoriteBlog(blogs)).toEqual(blogs[2])
+})

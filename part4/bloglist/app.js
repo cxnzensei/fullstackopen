8db991fef07c36724d0ea695ec7e9b3ac6b96dc8
cxnzensei <nishitpatil3231@gmail.com>
2022-01-17 07:52:27 +0530

    changes/refactors

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -10,9 +10,9 @@ const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
-logger.info('connecting to MongoDB >>>>>>>>>',)
+logger.info('connecting to MongoDB >>>>>>>>>')
 
-mongoose.connect(config.MONGODB_URI)
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     logger.info('connected to MongoDB.')
   })
@@ -22,13 +22,13 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(middleware.tokenExtractor)
 app.use(cors())
-app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use(loginRouter)
 app.use(usersRouter)
 app.use(blogsRouter)
+
 
 if (process.env.NODE_ENV === 'development') {
   const testingRouter = require('./controllers/testing')
